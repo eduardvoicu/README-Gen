0a82e878e4b37c7f0b6fd84c8a716d25207a1cf7
Eduard Voicu <iameduardvoicu@gmail.com>
2021-09-27 07:24:12 -0500

    adding array questions for user input.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+var inquirer =  require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    "type": "input",
+    "name": "title",
+    "message": "What is your project title?",
+},
+{
+    "type": "input",
+    "name": "description",
+    "message": "What is your project about?",
+},
+{
+    "type": "input",
+    "name": "installInstruction",
+    "message": "Provide installation instructions for your application.",
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
